6f31b3db6f50300b0247c1019bc0a0c08ba48775
itsercleung <leung.eric24@gmail.com>
2019-10-04 19:49:54 +1300

    Updated tilemap + added breaking ice blocks (with animation)

---
+++ LifeOfWilbur/Assets/Scripts/BreakBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBlock : MonoBehaviour
+{
+    public GameObject gameObject;
+    public Animator animator;
+    public float delayTime; //Num seconds
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Wilbur")
+        {
+            animator.SetBool("IsBreak", true);
+            StartCoroutine(HideObject(gameObject, delayTime));
+        }
+    }
+
+    IEnumerator HideObject(GameObject gameObject, float delayTime)
+    {
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(delayTime);
+        gameObject.SetActive(false);
+    }
+}
