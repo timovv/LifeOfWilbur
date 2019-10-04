79336dfbc43d6335c06c0b5757e9da68e9a520c9
Harman Lamba <harrygth15478@gmail.com>
2019-10-05 11:06:07 +1300

    Fixed rough implementation of exit scene and added the scripts to the components

---
+++ LifeOfWilbur/Assets/Scripts/ExitMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExitMenu : MonoBehaviour
+{
+    public TextMeshProUGUI TimeText;
+    public void PlayAgain()
+    {
+        //Loading the Game again, and not the menu screen
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the Game");
+        Application.Quit();
+    }
+
+    public void SetTime(float f)
+    {
+        TimeText.text = f.ToString();
+    }
+
+}
