12264dd66f536648cfdfd4b3c56b53001d4b1bc3
Harman Lamba <harrygth15478@gmail.com>
2019-10-05 09:53:44 +1300

    Rough Implementation of the Menu Screen, color and aesthetics to be changed

---
+++ LifeOfWilbur/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //Queue the next scene in the build order
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   public void QuitGame()
+    {
+        //TODO: Remove Debug Statement
+        //Testing/Making sure that the quit command is thrown
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
