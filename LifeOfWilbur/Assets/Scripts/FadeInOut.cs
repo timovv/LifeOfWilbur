96275bf2751f91ce50bb6c497051be7a404d8efd
Timo van Veenendaal <me@timo.nz>
2019-10-06 01:02:13 +1300

    Documentation for time travel mechanic

--- LifeOfWilbur/Assets/Scripts/FadeInOut.cs
+++ LifeOfWilbur/Assets/Scripts/FadeInOut.cs
@@ -5,10 +5,24 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Behaviour that can be applied to a game object with a raw image component
+/// that allows for it to be faded in and out as needed.
+/// 
+/// This is used to fade out the scene.
+/// </summary>
 [RequireComponent(typeof(RawImage))]
 public class FadeInOut : MonoBehaviour
 {
+    /// <summary>
+    /// How long in seconds it should take for the fade transition to occur.
+    /// This time is for each way, i.e. it will take this long to fade in, and this long again to fade out.
+    /// </summary>
     public float _fadeDurationSeconds;
+
+    /// <summary>
+    /// Records if we are currently faded in or out.
+    /// </summary>
     private bool _fadedOut;
 
     public void Start()
@@ -17,6 +31,10 @@ public class FadeInOut : MonoBehaviour
         image.CrossFadeAlpha(0f, 0, true);
     }
 
+    /// <summary>
+    /// Coroutine fade to black.
+    /// </summary>
+    /// <returns>Enumerator to be passed to StartCoroutine</returns>
     public IEnumerator FadeOutToBlack()
     {
         if(_fadedOut)
@@ -25,15 +43,16 @@ public class FadeInOut : MonoBehaviour
         }
 
         _fadedOut = true;
-        //StopCoroutine("FadeInFromBlack");
-
-        _fadedOut = true;
 
         var image = GetComponent<RawImage>();
         image.CrossFadeAlpha(alpha: 1f, duration: _fadeDurationSeconds * .75f, ignoreTimeScale: true);
         yield return new WaitForSecondsRealtime(_fadeDurationSeconds);
     }
 
+    /// <summary>
+    /// Coroutine to fade the scene back in from black.
+    /// </summary>
+    /// <returns>Enumerator to be passed to StartCoroutine</returns>
     public IEnumerator FadeInFromBlack()
     {
         if(!_fadedOut)
@@ -42,7 +61,6 @@ public class FadeInOut : MonoBehaviour
         }
 
         _fadedOut = false;
-        //StopCoroutine("FadeOutToBlack");
 
         var image = GetComponent<RawImage>();
         image.CrossFadeAlpha(alpha: 0f, duration: _fadeDurationSeconds * .75f, ignoreTimeScale: true);
