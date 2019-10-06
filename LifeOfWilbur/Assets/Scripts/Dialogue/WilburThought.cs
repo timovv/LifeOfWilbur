105fa0b64b02ad85888f3f343ea0fc86f3f96db2
JainalGandhi <jainalgandhi@gmail.com>
2019-10-06 21:49:08 +1300

    Implemented Wilbur Internal Monologue Issue #20

---
+++ LifeOfWilbur/Assets/Scripts/Dialogue/WilburThought.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilburThought : MonoBehaviour
+{
+    private bool playerHasRead;
+    public DialogueTrigger dialogueTrigger;
+
+    // Player has entered collision area and is now in range for thought prompt
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player") && !playerHasRead && !DialogueController.Instance.IsOpen) {
+            dialogueTrigger.TriggerDialogue();
+            playerHasRead = true;
+        }
+    }
+}
