3b7542e7a2a8e469c88c321979aceee444743a33
JainalGandhi <jainalgandhi@gmail.com>
2019-10-08 01:57:02 +1300

    Documented Dialogue Code

--- LifeOfWilbur/Assets/Scripts/Dialogue/NPC.cs
+++ LifeOfWilbur/Assets/Scripts/Dialogue/NPC.cs
@@ -2,41 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls the logic behind the NPC sprites. Utilises collision detection and keypress checks to check if the tooltip and DialogueWindow should show
+/// </summary>
 public class NPC : MonoBehaviour {
     
-    private bool inRange;
-    private bool playerHasRead;
-    public DialogueTrigger dialogueTrigger;
-    public Animator animator;
+    /// <summary>
+    /// Whether the player is in range
+    /// </summary>
+    private bool _inRange;
+
+    /// <summary>
+    /// Whether the player has already read the dialogue
+    /// </summary>
+    private bool _playerHasRead;
+
+    /// <summary>
+    /// The trigger which begins the conversation on the DialogueWindow. Also holds the actual conversation set in Unity.
+    /// </summary>
+    public DialogueTrigger _dialogueTrigger;
+
+    /// <summary>
+    /// The animator used to control the display of the tooltip
+    /// </summary>
+    public Animator _animator;
 
 
     private void Start() {
-        playerHasRead = false;
-        animator.SetBool("showToolTip", false);
+        _playerHasRead = false;
+        _animator.SetBool("showToolTip", false);
     }
 
-    // Opens the dialogueWindow if player in range (collision detection), user pressing "X" and dialogueWindow is not already open
+    /// <summary>
+    /// Opens the dialogueWindow if player in range (collision detection), user pressing "X" and dialogueWindow is not already open
+    /// </summary>
     void Update() {
-        if (!playerHasRead && inRange && Input.GetKeyDown(KeyCode.C) && !DialogueController.Instance.IsOpen) {
-            dialogueTrigger.TriggerDialogue();
-            playerHasRead = true;
-            animator.SetBool("showToolTip", false);
+        if (!_playerHasRead && _inRange && Input.GetKeyDown(KeyCode.C) && !DialogueController.Instance.IsOpen) {
+            _dialogueTrigger.TriggerDialogue();
+            _playerHasRead = true;
+            _animator.SetBool("showToolTip", false);
         }
     }
 
-    // Player has entered collision area and is now in range for starting dialogue
+    /// <summary>
+    /// Player has entered collision area and is now in range for starting dialogue
+    /// </summary>
+    /// <param name="other">The object which has entered the collision region</param>
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            animator.SetBool("showToolTip", !playerHasRead);
-            inRange = true;
+            _animator.SetBool("showToolTip", !_playerHasRead);
+            _inRange = true;
         }
     }
 
-    // Player has exited collision area and is not in range for starting dialogue
+    /// <summary>
+    /// Player has exited collision area and is now not in range for starting dialogue
+    /// </summary>
+    /// <param name="other">The object which has exited the collision region</param>
     void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            animator.SetBool("showToolTip", false);
-            inRange = false;
+            _animator.SetBool("showToolTip", false);
+            _inRange = false;
         }
         
     }
--- LifeOfWilbur/Assets/Scripts/Dialogue/WilburThought.cs
+++ LifeOfWilbur/Assets/Scripts/Dialogue/WilburThought.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls the logic behind the internal monolgue. Utilises collision detection and keypress checks to check if the tooltip and DialogueWindow should show
+/// </summary>
 public class WilburThought : MonoBehaviour
 {
-    private bool playerHasRead;
-    public DialogueTrigger dialogueTrigger;
+    /// <summary>
+    /// Whether the player has already read the dialogue
+    /// </summary>
+    private bool _playerHasRead;
 
-    // Player has entered collision area and is now in range for thought prompt
+    /// <summary>
+    /// The trigger which begins the conversation on the DialogueWindow. Also holds the actual conversation set in Unity.
+    /// </summary>
+    public DialogueTrigger _dialogueTrigger;
+
+    /// <summary>
+    /// Player has entered collision area and is now in range for starting dialogue
+    /// </summary>
+    /// <param name="other">The object which has entered the collision region</param>
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player") && !playerHasRead && !DialogueController.Instance.IsOpen) {
-            dialogueTrigger.TriggerDialogue();
-            playerHasRead = true;
+        if (other.CompareTag("Player") && !_playerHasRead && !DialogueController.Instance.IsOpen) {
+            _dialogueTrigger.TriggerDialogue();
+            _playerHasRead = true;
         }
     }
 }
