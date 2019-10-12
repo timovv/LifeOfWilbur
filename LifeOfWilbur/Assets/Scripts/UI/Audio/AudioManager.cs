25b3b53db414de97c8d3813321324c4e2ec713aa
Harman Lamba <harrygth15478@gmail.com>
2019-10-12 18:35:45 +1300

    Implemented Audio manager and prefabed it into the Level Prefab

---
+++ LifeOfWilbur/Assets/Scripts/UI/Audio/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] _sounds;
+
+
+
+
+    public static AudioManager _instance;
+
+    //Use this for initialisation
+    void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        }else {
+            Destroy(gameObject);
+            return;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in _sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s._clip;
+
+            s.source.volume = s._volume;
+            s.source.pitch = s._volume;
+            s.source.loop = s._loop;
+
+        }
+    }
+
+
+    void Start(){
+
+        Play("BackgroundMusic");
+
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(_sounds, sound => sound._name == name);
+        try
+        {
+            s.source.Play();
+        }catch(NullReferenceException e){
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+        
+    }
+}
---
+++ LifeOfWilbur/Assets/Scripts/UI/Audio/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string _name;
+    public AudioClip _clip;
+
+    [Range(0f, 1f)]
+    public float _volume;
+
+    [Range(0.1f, 3f)]
+    public float _pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    public bool _loop;
+}
