584fb432d65901de46c8f79759594ecda6ac47a8
itsercleung <leung.eric24@gmail.com>
2019-10-13 02:02:15 +1300

    Added level selection script

---
+++ LifeOfWilbur/Assets/Scripts/UI/LevelSelectScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelectScript : MonoBehaviour
+{
+    public GameObject _levelHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SelectLevel(""); 
+    }
+
+    private void SelectLevel(string levelName)
+    {
+        SceneManager.LoadScene(1);
+
+        //Setting the GamerTime back to 0 in the case of restarting the game
+        GameTimer.ElapsedTimeSeconds = 0;
+    }
+}
