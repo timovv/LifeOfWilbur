ababfd2669e84f6645c2c168cac72baaa3c1e9b8
Harman Lamba <harrygth15478@gmail.com>
2019-10-19 01:55:58 +1300

    Initial implementation of the level indicitator, currently the panel just gets deactivated. Animations to be added for a fade

---
+++ LifeOfWilbur/Assets/Scripts/UI/LevelIndicator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelIndicator : MonoBehaviour
+{
+    //TODO: Potentially delete the animator
+
+    //public Animator _animator;
+    public GameObject _levelIndicatorPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelIndicatorPanel.SetActive(true);
+        //_animator.SetBool("IsHidden", false);
+    }
+
+   public void Hide()
+    {
+        Debug.Log("Hide Run");
+       // _animator.SetBool("IsHidden", true);
+    }
+
+
+    public void HidePanel()
+    {
+        Debug.Log("Hide Panel Run");
+        //StartCoroutine(WaitAndHidePanel());
+    }
+
+
+    public IEnumerator WaitAndHidePanel()
+    {
+        Debug.Log("Hide wait");
+        yield return new WaitForSeconds(15);
+        _levelIndicatorPanel.SetActive(false);
+    }
+
+}
