447333246beabb999da2492ce0f9464e341c9623
Harman Lamba <harrygth15478@gmail.com>
2019-10-19 02:08:29 +1300

    Added in methods to set the text level, for each level

--- LifeOfWilbur/Assets/Scripts/UI/LevelIndicator.cs
+++ LifeOfWilbur/Assets/Scripts/UI/LevelIndicator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LevelIndicator : MonoBehaviour
@@ -8,6 +9,7 @@ public class LevelIndicator : MonoBehaviour
 
     //public Animator _animator;
     public GameObject _levelIndicatorPanel;
+    public TextMeshProUGUI _levelText;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,22 @@ public class LevelIndicator : MonoBehaviour
         //_animator.SetBool("IsHidden", false);
     }
 
-    public IEnumerator WaitAndHidePanel()
+
+    public IEnumerator SetUpPanel(string levelText)
     {
+        SetLevelText(levelText);
         yield return new WaitForSeconds(15);
+        HidePanel();
+    }
+
+    private void HidePanel()
+    {
         _levelIndicatorPanel.SetActive(false);
+    }
+
+    private void SetLevelText(string levelText)
+    {
+        _levelText.SetText(levelText);
     }
 
 }
