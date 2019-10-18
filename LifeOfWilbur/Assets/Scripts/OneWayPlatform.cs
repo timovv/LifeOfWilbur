a2629b4e8ecafa9aa14a590710e75b5ec2159177
JainalGandhi <jainalgandhi@gmail.com>
2019-10-19 12:53:47 +1300

    Added press arrow-down to fall through platform

--- LifeOfWilbur/Assets/Scripts/OneWayPlatform.cs
+++ LifeOfWilbur/Assets/Scripts/OneWayPlatform.cs
@@ -7,6 +7,8 @@ public class OneWayPlatform : MonoBehaviour
 
     public BoxCollider2D _selfCollider;
 
+    public PlatformEffector2D _platformEffector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,18 @@ public class OneWayPlatform : MonoBehaviour
 
         Physics2D.IgnoreCollision(_selfCollider, GameObject.Find("OldWilburPlaceholder").GetComponent<BoxCollider2D>());
         Physics2D.IgnoreCollision(_selfCollider, GameObject.Find("YoungWilburPlaceholder").GetComponent<BoxCollider2D>());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _platformEffector.rotationalOffset = 180f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            _platformEffector.rotationalOffset = 0;
+        }
     }
 }
