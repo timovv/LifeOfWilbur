42e609526355fb71a5caef03aea1c16c037ebc43
JainalGandhi <jainalgandhi@gmail.com>
2019-10-19 12:44:59 +1300

    Basic one-way-platform implemented

---
+++ LifeOfWilbur/Assets/Scripts/OneWayPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlatform : MonoBehaviour
+{
+
+    public BoxCollider2D _selfCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("OldWilburPlaceholder").GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(_selfCollider, GameObject.Find("OldWilburPlaceholder").GetComponent<BoxCollider2D>());
+        Physics2D.IgnoreCollision(_selfCollider, GameObject.Find("YoungWilburPlaceholder").GetComponent<BoxCollider2D>());
+    }
+}
