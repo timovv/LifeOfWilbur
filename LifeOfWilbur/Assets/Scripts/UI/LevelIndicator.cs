6caf044a9a006f716a6ac8c0131a513e5cba5eb3
Harman Lamba <harrygth15478@gmail.com>
2019-10-19 13:51:00 +1300

    Abstracted fade variables into a constant, changed timings and overall look and feel

--- LifeOfWilbur/Assets/Scripts/UI/LevelIndicator.cs
+++ LifeOfWilbur/Assets/Scripts/UI/LevelIndicator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class LevelIndicator : MonoBehaviour
 {
+    private const double FADETIME_COEFFICIENT =  0.5;
+
     public GameObject _levelIndicatorPanel;
     public TextMeshProUGUI _levelText;
     public CanvasGroup _canvasGroup;
@@ -17,32 +19,47 @@ public class LevelIndicator : MonoBehaviour
         _canvasGroup.alpha = 1;
     }
 
+    /// <summary>
+    /// Method thats starts the fading of the canvas group. Reduces the alpha of the canvas group based on the game
+    /// time. Note there is a fade time coefficient that can be modified to change the speed of the fade.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator StartFade()
     {
         while(_canvasGroup.alpha > 0) 
         {
-            _canvasGroup.alpha -= Time.deltaTime  / 2;
-            yield return null;
+            _canvasGroup.alpha -= (float)(Time.deltaTime  / FADETIME_COEFFICIENT);
+            yield return null; //Waiting for the next frame
         }
-        _canvasGroup.interactable = false;
+        _canvasGroup.interactable = false; //Ensure that everything in the canvas is no longer interactable
         yield return null;
     }
 
 
     public IEnumerator SetUpPanel(string levelText)
     {
-        SetLevelText(levelText);
-        yield return new WaitForSeconds(12);
+        SetLevelText(levelText); //Preset the text for the levels
+        yield return new WaitForSeconds(13); //Display the text for a bit and then start the fade and the hiding of the panel
         StartCoroutine(StartFade());
         StartCoroutine(HidePanel());
     }
 
+    /// <summary>
+    /// Deactivate the panel from the scene. It is already faded out at this point, this coroutine ensures that the panel
+    /// is no longer present in the scene
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator HidePanel()
     {
-        yield return new WaitForSeconds(15);
+        yield return new WaitForSeconds(15); // Wait for the fade animation to finish before deactivating the panel
         _levelIndicatorPanel.SetActive(false);
     }
 
+    /// <summary>
+    /// Setting the text of the level. This was made private as to abstract this from other methods being able to set the text,
+    /// without ensuruing that the panel will fade and hide. 
+    /// </summary>
+    /// <param name="levelText"></param>
     private void SetLevelText(string levelText)
     {
         _levelText.SetText(levelText);
