da0bffeea64f9d985c176d07a7b1bb3fb43b1bea
JainalGandhi <jainalgandhi@gmail.com>
2019-10-21 10:21:24 +1300

    Created p cheat code to go to next level

---
+++ LifeOfWilbur/Assets/Scripts/Level/CheatCodes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Defines cheat codes for testers to get rhugh levels faster. The cheat codes are as follows:
+///     Press P to go to the next room automatically
+/// </summary>
+public class CheatCodes : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Go to next level if "P" pressed
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            // Re-enables movement/physcis in case player was in dialogue which locks them
+            CharacterController2D.MovementDisabled = false; // enable Wilbur's movement
+            TimeTravelController.TimeTravelDisabled = false; // enable Time Travel
+            LevelReset.ResetDisabled = false; // enable resetting level
+            Physics2D.autoSimulation = true; // enable physcis
+            
+            // Goes to next level
+            LifeOfWilbur.LevelController.NextRoom();
+        }
+    }
+}
