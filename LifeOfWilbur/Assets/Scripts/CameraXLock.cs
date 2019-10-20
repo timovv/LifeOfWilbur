daff22bcaaa3a4fbdb6315d5c62d777d852b0e50
JainalGandhi <jainalgandhi@gmail.com>
2019-10-20 16:25:31 +1300

    Commented Camera Scripts

--- LifeOfWilbur/Assets/Scripts/CameraXLock.cs
+++ LifeOfWilbur/Assets/Scripts/CameraXLock.cs
@@ -9,14 +9,21 @@ using Cinemachine;
 [AddComponentMenu("")] // Hide in menu
 public class CameraXLock : CinemachineExtension
 {
+    /// <summary>
+    /// The initial x position of the camera
+    /// </summary>
     public float XPosition;
 
+    /// <summary>
+    /// Overrides the PostPipelineStageCallback using the template pattern to remove any x-position effect
+    /// </summary>
     protected override void PostPipelineStageCallback(
         CinemachineVirtualCameraBase vcam,
         CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
         if (stage == CinemachineCore.Stage.Body)
         {
+            // Resets the new updated x position to XPosition
             var pos = state.RawPosition;
             pos.x = XPosition;
             state.RawPosition = pos;
