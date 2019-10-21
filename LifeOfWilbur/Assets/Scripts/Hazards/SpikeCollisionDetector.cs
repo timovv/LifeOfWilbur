98c51144926ab8d481873cc42f520bcde36926d0
William <williamlixu@gmail.com>
2019-10-21 19:33:17 +1300

    Remove pausing physics on death

--- LifeOfWilbur/Assets/Scripts/Hazards/SpikeCollisionDetector.cs
+++ LifeOfWilbur/Assets/Scripts/Hazards/SpikeCollisionDetector.cs
@@ -24,41 +24,7 @@ public class SpikeCollisionDetector : MonoBehaviour
             screenShake.Shake(duration);
             FindObjectOfType<AudioManager>().Play("DeathSFX");
             _animator.SetTrigger("isDeath");
-            StartCoroutine(PauseMovementOnDeath());
             LifeOfWilbur.LevelController.ResetLevel();
         }
-    }
-
-    /// <summary>
-    /// Pauses movement, kills, and then reenables. This is so Wilbur can't keep moving after he dies.
-    /// </summary>
-    IEnumerator PauseMovementOnDeath()
-    {
-        DisableMovement();
-        yield return new WaitForSeconds(0.1f);
-        EnableMovement();
-    }
-
-
-    /// <summary>
-    /// Disables movement and physics
-    /// </summary>
-    void DisableMovement()
-    {
-        CharacterController2D.MovementDisabled = true; // disable Wilbur's movement
-        TimeTravelController.TimeTravelDisabled = true; // disable Time Travel
-        LevelReset.ResetDisabled = true; // disable resetting level
-        Physics2D.autoSimulation = false; // disable physics
-    }
-
-    /// <summary>
-    /// Ren-enables movement and physics
-    /// </summary>
-    void EnableMovement()
-    {
-        CharacterController2D.MovementDisabled = false; // enable Wilbur's movement
-        TimeTravelController.TimeTravelDisabled = false; // enable Time Travel
-        LevelReset.ResetDisabled = false; // enable resetting level
-        Physics2D.autoSimulation = true; // enable physcis
     }
 }
